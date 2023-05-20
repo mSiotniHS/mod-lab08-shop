93e53d0730e30888737afc76d878ab8a27aa2a6d
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-20 11:26:47 +0300

    clean: Форматирование файла

--- include/CustomerSpawner.h
+++ include/CustomerSpawner.h
@@ -13,8 +13,8 @@ private:
 	std::default_random_engine randomEngine;
 	std::uniform_int_distribution<int> dist;
 
-	static double getLeftBoundary(double expectedValue, double variance) ;
-	static double getRightBoundary(double expectedValue, double variance) ;
+	static double getLeftBoundary(double expectedValue, double variance);
+	static double getRightBoundary(double expectedValue, double variance);
 
 public:
 	CustomerSpawner(Handler handler, unsigned int seed, double expectedValue, double variance);
