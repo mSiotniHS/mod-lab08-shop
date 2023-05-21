b73ea9f51f318decf8ee71035537439092146f0f
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-21 22:57:24 +0300

    Убрал try-catch блоки

--- test/tests.cpp
+++ test/tests.cpp
@@ -134,53 +134,39 @@ TEST(CheckoutTests, scenario1)
 //region Shop Tests
 TEST(ShopTests, scenario1)
 {
-	try {
-		Shop shop(2, milliseconds(500), 2);
-		EXPECT_THAT(shop.isWorking(), Eq(true));
+	Shop shop(2, milliseconds(500), 2);
+	EXPECT_THAT(shop.isWorking(), Eq(true));
 
-		shop.stopIfWorking();
+	shop.stopIfWorking();
 
-		EXPECT_THAT(shop.isWorking(), Eq(false));
+	EXPECT_THAT(shop.isWorking(), Eq(false));
 
-		auto data = shop.getData();
+	auto data = shop.getData();
 
-		EXPECT_THAT(data.rejectedCustomerCount, Eq(0));
-		EXPECT_THAT(data.acceptedCustomerCount, Eq(0));
-	} catch (const std::system_error& e) {
-		std::cout << "Caught system_error with code " << e.code()
-		          << " meaning " << e.what() << '\n';
-	} catch (const std::exception& e) {
-		std::cout << "Caught generic error: " << e.what() << '\n';
-	}
+	EXPECT_THAT(data.rejectedCustomerCount, Eq(0));
+	EXPECT_THAT(data.acceptedCustomerCount, Eq(0));
 }
 
 TEST(ShopTests, scenario2)
 {
-	try {
-		Shop shop(2, milliseconds(500), 2);
+	Shop shop(2, milliseconds(500), 2);
 
-		shop.handleCustomer(std::make_shared<Customer>(Customer(1, 5)));
-		std::this_thread::sleep_for(milliseconds(100));
-		shop.handleCustomer(std::make_shared<Customer>(Customer(2, 5)));
-		std::this_thread::sleep_for(milliseconds(100));
-		shop.handleCustomer(std::make_shared<Customer>(Customer(3, 5)));
-		std::this_thread::sleep_for(milliseconds(100));
-		shop.handleCustomer(std::make_shared<Customer>(Customer(4, 5)));
-		std::this_thread::sleep_for(milliseconds(100));
-		shop.handleCustomer(std::make_shared<Customer>(Customer(5, 5)));
+	shop.handleCustomer(std::make_shared<Customer>(Customer(1, 5)));
+	std::this_thread::sleep_for(milliseconds(100));
+	shop.handleCustomer(std::make_shared<Customer>(Customer(2, 5)));
+	std::this_thread::sleep_for(milliseconds(100));
+	shop.handleCustomer(std::make_shared<Customer>(Customer(3, 5)));
+	std::this_thread::sleep_for(milliseconds(100));
+	shop.handleCustomer(std::make_shared<Customer>(Customer(4, 5)));
+	std::this_thread::sleep_for(milliseconds(100));
+	shop.handleCustomer(std::make_shared<Customer>(Customer(5, 5)));
 
-		shop.stopIfWorking();
+	shop.stopIfWorking();
 
-		auto data = shop.getData();
+	auto data = shop.getData();
 
-		EXPECT_THAT(data.acceptedCustomerCount, Eq(4));
-		EXPECT_THAT(data.rejectedCustomerCount, Eq(1));
-		EXPECT_THAT((double) std::reduce(data.queueSizeSamples.begin(), data.queueSizeSamples.end()) / data.queueSizeSamples.size(), DoubleNear(1.8, 0.2));
-	} catch (const std::system_error& e) {
-		std::cout << "Caught system_error with code " << e.code()
-		          << " meaning " << e.what() << '\n';
-	} catch (const std::exception& e) {
-		std::cout << "Caught generic error: " << e.what() << '\n';
-	}
+	EXPECT_THAT(data.acceptedCustomerCount, Eq(4));
+	EXPECT_THAT(data.rejectedCustomerCount, Eq(1));
+	EXPECT_THAT((double) std::reduce(data.queueSizeSamples.begin(), data.queueSizeSamples.end()) / data.queueSizeSamples.size(), DoubleNear(1.8, 0.2));
 }
 //endregion
